136f08aef2fd6fd8d9d72d25b2f021a79226cdcc
Mirror83 <glenochieng045@gmail.com>
2023-07-18 17:39:09 +0300

    Modified MainActivity to use BookshelfApp

--- app/src/main/java/com/example/bookshelf/MainActivity.kt
+++ app/src/main/java/com/example/bookshelf/MainActivity.kt
@@ -4,43 +4,43 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.CenterAlignedTopAppBar
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.res.stringResource
+import com.example.bookshelf.ui.BookshelfApp
 import com.example.bookshelf.ui.theme.BookshelfTheme
 
 class MainActivity : ComponentActivity() {
+    @OptIn(ExperimentalMaterial3Api::class)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             BookshelfTheme {
                 // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
-                ) {
-                    Greeting("Android")
+                Scaffold(topBar = { BookshelfTopAppBar() }) { paddingValues ->
+                    Surface(
+                        modifier = Modifier
+                            .fillMaxSize()
+                             .padding(paddingValues),
+                        color = MaterialTheme.colorScheme.background,
+                    ) {
+                        BookshelfApp()
+                    }
                 }
             }
         }
     }
 }
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    BookshelfTheme {
-        Greeting("Android")
-    }
+fun BookshelfTopAppBar(modifier: Modifier = Modifier) {
+    CenterAlignedTopAppBar(title = { Text(stringResource(R.string.app_name)) }, modifier = modifier)
 }
