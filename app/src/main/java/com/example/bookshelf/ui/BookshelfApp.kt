582ff329c2bf1e94290b54383d346bfe6f9f3aae
Mirror83 <glenochieng045@gmail.com>
2023-07-20 11:24:30 +0300

    Modified viewModel to use Factory and added simple loading animation

--- app/src/main/java/com/example/bookshelf/ui/BookshelfApp.kt
+++ app/src/main/java/com/example/bookshelf/ui/BookshelfApp.kt
@@ -1,7 +1,10 @@
 package com.example.bookshelf.ui
 
 
+import androidx.compose.animation.core.Animatable
+import androidx.compose.animation.core.tween
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.aspectRatio
@@ -18,25 +21,32 @@ import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.drawBehind
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.platform.LocalFocusManager
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.input.ImeAction
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.lifecycle.viewmodel.compose.viewModel
 import coil.compose.AsyncImage
 import coil.request.ImageRequest
 import com.example.bookshelf.R
+import kotlinx.coroutines.coroutineScope
+import kotlinx.coroutines.launch
 
 @Composable
 fun BookshelfApp(
     modifier: Modifier = Modifier,
-    bookshelfViewModel: BookshelfViewModel = viewModel(),
+    bookshelfViewModel: BookshelfViewModel = viewModel(factory = BookshelfViewModel.Factory),
 ) {
     val bookshelfUiState by
     bookshelfViewModel.bookshelfUiState.collectAsState()
@@ -94,6 +104,7 @@ fun BookshelfSearchBar(
     val focusManager = LocalFocusManager.current
     Row(
         verticalAlignment = Alignment.CenterVertically,
+        horizontalArrangement = Arrangement.Center,
         modifier = modifier
             .padding(8.dp)
             .fillMaxWidth()
@@ -140,12 +151,55 @@ fun BookThumbnail(thumbnailUrl: String, modifier: Modifier = Modifier) {
 
 @Composable
 fun LoadingScreen(modifier: Modifier = Modifier) {
+    val alpha1 = remember { Animatable(0f) }
+    val alpha2 = remember { Animatable(0f) }
+    val alpha3 = remember { Animatable(0f) }
+    LaunchedEffect(Unit) {
+        while (true) {
+            coroutineScope {
+                launch {
+                    alpha1.animateTo(1f, tween(500))
+                }
+                launch {
+                    alpha2.animateTo(1f, tween(500, 100))
+                }
+                launch {
+                    alpha3.animateTo(1f, tween(500, 250))
+                }
+            }
+            coroutineScope {
+                alpha3.animateTo(0f, tween(500))
+                alpha2.animateTo(0f, tween(500))
+                alpha1.animateTo(0f, tween(500))
+            }
+        }
+    }
     Column(
         verticalArrangement = Arrangement.Center,
         horizontalAlignment = Alignment.CenterHorizontally,
         modifier = modifier.fillMaxSize()
     ) {
-        Text(stringResource(R.string.loading))
+        Text("Loading", modifier = Modifier.padding(8.dp))
+        Row(horizontalArrangement = Arrangement.Center) {
+            LoadingBubble(alpha = alpha1.value)
+            LoadingBubble(color = Color.Red, alpha = alpha2.value)
+            LoadingBubble(alpha = alpha3.value)
+        }
+    }
+}
+
+@Composable
+fun LoadingBubble(
+    modifier: Modifier = Modifier,
+    color: Color = Color.LightGray,
+    alpha: Float = 1f,
+) {
+    Box(modifier = modifier
+        .padding(12.dp)
+        .drawBehind {
+            drawCircle(color = color, radius = 8.dp.toPx(), alpha = alpha)
+        }) {
+
     }
 }
 
@@ -172,4 +226,19 @@ fun ErrorScreen(
             Text(stringResource(R.string.retry))
         }
     }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun BookshelfAppPreview() {
+    Column(
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center,
+        modifier = Modifier
+            .padding(4.dp)
+            .fillMaxSize()
+    ) {
+        LoadingScreen()
+    }
+
 }
